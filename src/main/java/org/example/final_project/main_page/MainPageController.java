6d26abb8daf47deb4aa6c7be85b1fab9cc549e37
QBach <qbach1010@gmail.com>
2026-06-22 15:17:14 +0700

    me may beo

--- src/main/java/org/example/final_project/main_page/MainPageController.java
+++ src/main/java/org/example/final_project/main_page/MainPageController.java
@@ -27,6 +27,7 @@ public class MainPageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setupMainPage();
+
     }
 
     private void setupMainPage() {
